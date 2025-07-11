5e4e811d8573339edf4f180e9cfeda0b67f7359c
Daniel Rys <danjrys@gmail.com>
2025-07-11 15:03:14 -0400

    Updated files for build support

--- build.sh
+++ build.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 
 current_dir=$(pwd)
+
+desktop_file_path="$current_dir/Audio-Share.desktop"
+build_exec_path="$current_dir/main.dist/main.bin"  # Or the built binary
+app_icon_path="$current_dir/main.dist/assets/icon.png"  # Optional
+
 venv_dir="$current_dir/.venv"
 
 if test -d "$venv_dir"; then
@@ -22,3 +27,36 @@ else
     echo -e "\nAfter installing dependencies, make sure \"as-cmd\" is included in the \"src\" folder"
     echo -e "\nIt can be downloaded at \"https://github.com/mkckr0/audio-share/releases\"\nmake sure download \"audio-share-server-cmd-linux.tar.gz\" and only extract the \"as-cmd\" file\n"
 fi
+echo -e "\n"
+while true; do
+    echo -e ""
+    read -p "Do you want to create the .desktop file? (y/n) " yn
+    case $yn in
+        [Yy]* ) echo -e "\nCreating .desktop file at $desktop_file_path";
+                cat > "$desktop_file_path" <<EOL
+[Desktop Entry]
+Type=Application
+Name=AudioShare
+Exec=$build_exec_path
+Icon=$app_icon_path
+Terminal=false
+Categories=Utility;
+EOL
+
+            chmod +x "$desktop_file_path"
+            break;;
+        [Nn]* ) echo -e "\nExiting..."; exit;;
+        * ) echo "Invalid input. Please answer yes or no.";;
+    esac
+done
+echo -e "\n"
+while true; do
+    read -p "Do you want to move the .desktop file to ($HOME/.local/share/applications)? (y/n) " yn
+    case $yn in
+        [Yy]* ) mv $desktop_file_path "$HOME/.local/share/applications/Audio-Share.desktop";
+                echo -e "\nExiting..."
+                break;;
+        [Nn]* ) echo -e "\nExiting..."; exit;;
+        * ) echo "Invalid input. Please answer yes or no.";;
+    esac
+done
